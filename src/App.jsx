69f92052eec128af0ccfa3d4b80df42000a71cdb
asseternity <nakupovasset@gmail.com>
2024-06-24 22:15:53 +0500

    feat: forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello!</h1>
+      <h3>Search for something:</h3>
+      <form>
+        <input type="text" value="Cats" />
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
+
+// remember how to use apis
+// learn how to use environmental variables
+// remember async await
